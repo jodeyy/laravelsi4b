2bd2e82767a020cf2a1cb35d0755301870baa78e
Jodi <jodiptra@gmail.com>
2024-05-02 15:16:36 +0700

    lavarel update

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+
+use App\Http\Controllers\FakultasController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,3 +15,25 @@ Route::get('about',function(){
 Route::get('profil',function(){
     return view('profil');
 });
+
+// route dengan parameter
+Route::get('welcome/{salam}',function ($salam){
+    // return 'selamat '.$salam;
+    return view('salam')->with('viewsalam', $salam);
+});
+
+// Route tanpa parameter
+// terdapat array $list
+Route::get('listdata', function () {
+    $list = ["Sistem Informasi","Informatika","Manajemen"];
+    $listmhs = [
+        ["npm" => 001, "nama" => "ahmad"],
+        ["npm" => 002, "nama" => "budi"]
+    ];
+    return view('listprodi')
+    ->with('viewlist', $list)
+    ->with('viewmhs', $listmhs);
+
+});
+
+Route::resource('fakultas',FakultasController::class);
